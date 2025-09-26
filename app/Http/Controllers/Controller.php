f049e0296d9d59a286d013d94fc47f8b9ff530fb
Juliano Mancini <juuliano.mancin@gmail.com>
2025-09-26 14:16:15 -0300

    comentario e organizcao do controller Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers; /* define o namespace do controller */
 
-abstract class Controller
+abstract class Controller /* classe base abstrata de onde todos os controllers do Laravel herdam */
 {
-    //
+    /* espaço reservado para métodos ou propriedades comuns aos controllers */
 }
